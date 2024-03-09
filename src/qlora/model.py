2b06ce8e01defc91b6257e159b7c2bcb38df5568
Prerana <prerana@hellosivi.com>
2024-03-09 20:41:06 +0530

    adding quantization

--- src/qlora/model.py
+++ src/qlora/model.py
@@ -1,5 +1,20 @@
-from transformers import AutoModelForCausalLM
+import torch
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+
+def quantization_config():
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+    )
+    return bnb_config
+
 
 def get_model():
-    model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        "microsoft/phi-2", 
+        torch_dtype="auto", 
+        quantization_config=quantization_config(),
+        trust_remote_code=True)
+    model.config.use_cache = False
     return model
